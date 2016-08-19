87254af474eeb5d5934d8ce2bca306e77971147c
wopqw <wopqw@yandex.ru>
2016-08-19 20:07:37 +0300

    second commit[2]

---
+++ intro/src/main/java/CommandArg.java
@@ -0,0 +1,10 @@
+/**
+ * Created by woqpw on 19.08.16.
+ */
+public class CommandArg {
+    public static void main(String[] args) {
+        for (int i = 0; i<args.length; i++ ){
+            System.out.println(args[i]);
+        }
+    }
+}
---
+++ intro/src/main/java/First.java
@@ -0,0 +1,8 @@
+/**
+ * Created by woqpw on 19.08.16.
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Java уже здесь");
+    }
+}
